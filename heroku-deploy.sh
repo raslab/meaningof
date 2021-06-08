8a4ecb66d639c2bfe0849b00502f1394738c6f38
Radomyr <radomyr.slaboshpitskyi@timecode.com>
2021-06-08 23:10:46 +0300

    udpate deploy scripts

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+cd back
+npm install
+cd ..
+
 cd front
+yarn install
 sed -i 's/http:\/\/localhost:2020\/api/https:\/\/meaning-of-web.herokuapp.com/\/api/g' ./src/config.js
 yarn build
 rm -rf ../back/public
